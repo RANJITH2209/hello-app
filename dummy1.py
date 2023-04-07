90e4cabb38bf9440f3c7f6a8ce51fa13ca75fdc3
RANJITH2209 <62751585+RANJITH2209@users.noreply.github.com>
2023-04-07 08:21:43 +0530

    Add files via upload

---
+++ dummy1.py
@@ -0,0 +1,22 @@
+#importing packages
+import streamlit as st
+from IPython.display import HTML
+import pandas as pd
+
+
+st.set_page_config(layout="wide")
+#st.title("Team 5")
+
+st.markdown("<h1 style='font-family: cursive; text-align: center;'>Team Senti5</h1>", unsafe_allow_html=True)
+st.markdown(
+    "<h1 style='text-align: center;'><img src='https://icons.iconarchive.com/icons/dtafalonso/android-lollipop/512/Youtube-icon.png' height='50'/> Smart Youtube Results</h1>",
+    unsafe_allow_html=True
+)
+
+st.markdown("<h2 style='text-align: right; font-size: 20px;'> ~ From the students of Praxis Business School, Bangalore</h2>", unsafe_allow_html=True)
+
+st.subheader("Search")
+topic = st.text_input("")
+
+if topic != "":
+  st.markdown("Fetching results, Please wait...")
