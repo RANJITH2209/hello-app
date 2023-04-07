037ee029130c3b11b48042943df92c9250b7e99b
RANJITH2209 <62751585+RANJITH2209@users.noreply.github.com>
2023-04-07 08:46:45 +0530

    Update dummy1.py

--- dummy1.py
+++ dummy1.py
@@ -20,3 +20,35 @@ topic = st.text_input("")
 
 if topic != "":
   st.markdown("Fetching results, Please wait...")
+
+width = 350
+height =200
+L = ["PHxYNGo8NcI","FUY07dvaUuE", "ZVR2Way4nwQ","PYxDkGlpj_U","JcI5E2Ng6r4","ydvnVw80I_8","RmajweUFKvM","_L39rN6gz7Y","coOTEc-0OGw"]
+
+col1, col2 = st.columns([1, 1])
+
+# Add content to columns
+
+name =["Machine Learning Tutorial Python - 9  Decision Tree","Decision Analysis 4 (Tree): EVSI - Expected Value of Sample Information",
+"Decision Tree Classification Clearly Explained!",
+"Decision Trees for A Level Business",
+"Decision Tree: Important things to know",
+"Decision Analysis 3: Decision Trees",
+"Decision Tree In Machine Learning | Decision Tree Algorithm In Python |Machine Learning |Simplilearn",
+"Decision and Classification Trees, Clearly Explained!!!",
+"1. Decision Tree | ID3 Algorithm | Solved Numerical Example | by Mahesh Huddar"]
+
+col1.header("Youtube Results")
+for i in range(9):
+		html_code = f'<iframe width="{width}" height={height}" src="https://www.youtube.com/embed/{L[i]}" frameborder="0" allowfullscreen></iframe>'
+		# Display HTML code
+		col1.markdown(html_code, unsafe_allow_html=True)
+		col1.subheader(name[i])
+
+col2.header("Smart Results")
+
+for i in range(9):
+		html_code = f'<iframe width="{width}" height={height}" src="https://www.youtube.com/embed/{L[i]}" frameborder="0" allowfullscreen></iframe>'
+		# Display HTML code
+		col2.markdown(html_code, unsafe_allow_html=True)
+		col2.subheader(name[i])
