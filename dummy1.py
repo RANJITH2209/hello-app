bdf59aa89dee240fa3061c8443c5a4c7f5d43c74
RANJITH2209 <62751585+RANJITH2209@users.noreply.github.com>
2023-04-07 11:09:41 +0530

    Update dummy1.py

--- dummy1.py
+++ dummy1.py
@@ -53,21 +53,23 @@ col1.header("FrameWork 1")
 col2.header("FrameWork 2")
 
 
+try:
+	for i in range(3):
 
-for i in range(3):
-	
-	col1, empty,col2 = st.columns(3)
+		col1, empty,col2 = st.columns(3)
 
-	youtube_vd_id1 = Meta_Data[topic][0][i] 
-	html_code1 = f'<iframe width="{width}" height={height}" src="https://www.youtube.com/embed/{youtube_vd_id1}" frameborder="1" allowfullscreen></iframe>'
-	col1.markdown(html_code1, unsafe_allow_html=True)
-	col1.subheader(Meta_Data[topic][1][i])
+		youtube_vd_id1 = Meta_Data[topic][0][i] 
+		html_code1 = f'<iframe width="{width}" height={height}" src="https://www.youtube.com/embed/{youtube_vd_id1}" frameborder="1" allowfullscreen></iframe>'
+		col1.markdown(html_code1, unsafe_allow_html=True)
+		col1.subheader(Meta_Data[topic][1][i])
 
 
-	youtube_vd_id2 = Meta_Data[topic][2][i]
-	html_code2 = f'<iframe width="{width}" height={height}" src="https://www.youtube.com/embed/{youtube_vd_id2}" frameborder="1" allowfullscreen></iframe>'
-	col2.markdown(html_code2, unsafe_allow_html=True)
-	col2.subheader(Meta_Data[topic][3][i])
+		youtube_vd_id2 = Meta_Data[topic][2][i]
+		html_code2 = f'<iframe width="{width}" height={height}" src="https://www.youtube.com/embed/{youtube_vd_id2}" frameborder="1" allowfullscreen></iframe>'
+		col2.markdown(html_code2, unsafe_allow_html=True)
+		col2.subheader(Meta_Data[topic][3][i])
+except:
+	st.write("Enter the search topic above")
 
 
 
